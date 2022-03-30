e1ac7e07544385298d74df528e77e8f42379a314
vmircea <vmirceamr@gmail.com>
2022-03-30 13:35:20 +0300

    Fix User Data

--- src/main/java/com/example/aop_master_project/model/entities/UserData.java
+++ src/main/java/com/example/aop_master_project/model/entities/UserData.java
@@ -7,7 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-@Entity
+@Entity(name = "User_Data")
 public class UserData {
 
     @Id
--- src/main/java/com/example/aop_master_project/repositories/UserDataRepository.java
+++ src/main/java/com/example/aop_master_project/repositories/UserDataRepository.java
@@ -7,9 +7,9 @@ import org.springframework.data.repository.query.Param;
 
 public interface UserDataRepository extends JpaRepository<UserData, String> {
 
-    @Query("select case when count(userData)> 0 then true else false end from UserData userData where lower(userData.username) = lower(:username)")
+    @Query("select case when count(userData)> 0 then true else false end from User_Data userData where lower(userData.username) = lower(:username)")
     public boolean usernameExists(@Param("username") String username);
 
-    @Query("select userData from UserData userData where lower(userData.username) = lower(:username)")
+    @Query("select userData from User_Data userData where lower(userData.username) = lower(:username)")
     public UserData userDataForUsername(@Param("username") String username);
 }
