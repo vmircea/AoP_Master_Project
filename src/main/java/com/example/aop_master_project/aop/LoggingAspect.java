0bf4a78edfde0fd6c5d3a18d4441a8a18130b193
Andrei Ionescu <andrei.ionescu@student.manchester.ac.uk>
2022-05-05 14:20:00 +0300

    Added Logging Aspect info

--- src/main/java/com/example/aop_master_project/aop/LoggingAspect.java
+++ src/main/java/com/example/aop_master_project/aop/LoggingAspect.java
@@ -1,11 +1,14 @@
 package com.example.aop_master_project.aop;
 
-import lombok.extern.slf4j.Slf4j;
+import com.example.aop_master_project.model.entities.LogsManager;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import sun.misc.Signal;
 
 import java.time.Duration;
 import java.time.Instant;
@@ -13,16 +16,45 @@ import java.time.Instant;
 @Aspect
 @Order(1)
 @Component
-@Slf4j
 public class LoggingAspect {
+
+    @Autowired
+    private LogsManager logsManager;
+
+    @Pointcut("execution(* com.example.aop_master_project.repositories.*.save*(..)) && args(o)")
+    public void logNewSave(Object o) {
+    }
+
+    @Pointcut("execution(* com.example.aop_master_project.repositories.*.delete*(..)) && args(o)")
+    public void logDelete(Object o) {
+    }
+
+    @After("logNewSave(o)")
+    public void afterAdvice(JoinPoint joinPoint, Object o) {
+        Signature signature = joinPoint.getSignature();
+        String message = signature.getName().toUpperCase() + " ";
+        logsManager.info(message, o.toString());
+    }
+
+    @After("logDelete(o)")
+    public void afterAdvice2(JoinPoint joinPoint, Object o) {
+        Signature signature = joinPoint.getSignature();
+        String message = signature.getName().toUpperCase() + " ";
+        logsManager.info(message, o.toString());
+    }
+
+    @Before("execution(private * com.example.aop_master_project.services.*.build*(..)) && args(o)")
+    public void beforeAdvice(Object o) {
+        System.out.println("Mapping object " + o.toString());
+    }
     @Around("execution(* com.example.aop_master_project.controllers.*.*(..))")
     public Object log(ProceedingJoinPoint joinPoint) throws Throwable {
-        log.info("Start endpoint {} .", joinPoint.getSignature());
+        logsManager.info("Start endpoint {} .", String.valueOf(joinPoint.getSignature()));
         Instant start = Instant.now();
         Object proceed = joinPoint.proceed();
         Instant finish = Instant.now();
         long timeElapsed = Duration.between(start, finish).toMillis();
-        log.info("End call after {} milliseconds.", timeElapsed);
+        logsManager.info("End call after {} milliseconds.", String.valueOf(timeElapsed));
         return proceed;
     }
 }
---
+++ src/main/java/com/example/aop_master_project/model/entities/LogsManager.java
@@ -0,0 +1,17 @@
+package com.example.aop_master_project.model.entities;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+
+@Configuration
+public class LogsManager {
+    public static void info(String message, String param) {
+        log.info(message, param);
+    }
+
+    public static void info(String message) {
+        log.info(message);
+    }
+}
